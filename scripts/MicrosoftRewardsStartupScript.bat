2bdf3514b3d4be6a4dfc99c19e30e08700e29ae6
Michael Savage <mpsavaye@gmail.com>
2022-11-05 11:20:05 -0700

    Update script to kill running edge processes

--- scripts/MicrosoftRewardsStartupScript.bat
+++ scripts/MicrosoftRewardsStartupScript.bat
@@ -1,2 +1,7 @@
+:: Add following script to startup folder
+:: Navigate using: 'windows key + R' -> 'shell:startup'
+:: Modify project directory
+
+taskkill /F /IM msedge.exe
 cd C:\Users\Micha\Projects\MicrosoftRewards\MicrosoftRewards
 npm run all
